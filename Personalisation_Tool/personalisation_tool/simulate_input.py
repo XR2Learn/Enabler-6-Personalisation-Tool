bca988ce9edd432b639934cde9cf0138dbea13aa
Annanda Sousa <annanda.sousa@gmail.com>
2023-10-24 11:55:56 +0200

    Created a simulated input as publisher and a subscriber to listen to the content.

--- Personalisation_Tool/personalisation_tool/simulate_input.py
+++ Personalisation_Tool/personalisation_tool/simulate_input.py
@@ -1,18 +1,63 @@
+import time
+
 import redis
 import json
 from personalisation_tool.conf import REDIS_HOST, REDIS_PORT
 
 
-def publish_input(redis_cli, event_type, event_data):
-    json_message = json.dumps(event_data)
-    redis_cli.publish(event_type, json_message)
+# def publish_input(redis_cli, event_type, event_data):
+#     json_message = json.dumps(event_data)
+#     result = redis_cli.publish(event_type, json_message)
+#     return result
+#
+#
+# def simulate_activity_session(redis_cli):
+#     event_type = 'start_activity'
+#     event_data = {
+#         'user_level': 0,  # 0 is beginner
+#         'activity_level': 0  # 0 is easy
+#     }
+#
+#     print(publish_input(redis_cli, event_type, event_data))
+
+
+class SimulateInput:
+    def __init__(self, redis_cli):
+        self.redis_cli = redis_cli
+
+    def run(self):
+        event_type = 'start_activity'
+        event_data = {
+            'id': 0,
+            'user_level': 0,  # 0 is beginner
+            'activity_level': 0  # 0 is easy
+        }
+        print(self.publish_simulated_activity_session(event_type, event_data))
+
+        for i in range(10):
+            event_type = 'emotion'
+            event_data = {
+                'emotion': i % 3,  # 1 is flow
+            }
+            print(self.publish_simulated_activity_session(event_type, event_data))
+            time.sleep(2)
+
+        event_type = 'end_activity'
+        event_data = {
+            'id': 0,
+            'timestamp': time.time()  # 1 is flow
+        }
+        print(self.publish_simulated_activity_session(event_type, event_data))
+
+    def publish_simulated_activity_session(self, event_type, event_data):
+        json_message = json.dumps(event_data)
+        result = redis_cli.publish(event_type, json_message)
+        return result
 
 
 if __name__ == '__main__':
     redis_cli = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
-    event_type = 'start_activity'
-    event_data = {
-        'user_level': 0,  # 0 is beginner
-        'activity_level': 0  # 0 is easy
-    }
-    publish_input(redis_cli, event_type, event_data)
+    # simulate_activity_session(redis_cli)
+
+    simulate_input = SimulateInput(redis_cli)
+    simulate_input.run()
--- Personalisation_Tool/personalisation_tool/suggest_activity_level.py
+++ Personalisation_Tool/personalisation_tool/suggest_activity_level.py
@@ -1,12 +1,38 @@
 # Your Python code here
+import redis
+from conf import MAIN_FOLDER, REDIS_PORT, REDIS_HOST
 
-from conf import MAIN_FOLDER
 
+class PersonalisationTool:
+    def __init__(self, redis_cli):
+        self.redis_cli = redis_cli
+        self.pubsub = self.redis_cli.pubsub()
+        # self.sub_event_types = ['start_activity', 'end_activity', 'emotion']
+        self.sub_event_types = {'start_activity': self.handle_start_activity,
+                                'end_activity': self.handle_end_activity,
+                                'emotion': self.handle_emotion}
 
-def call_component():
-    print(MAIN_FOLDER)
-    print('Call component Personalisation Tool')
+    def recommend_level(self):
+        return 1
+
+    def run(self):
+        self.pubsub.subscribe(**self.sub_event_types)
+        for message in self.pubsub.listen():
+            # print(message)
+            pass
+
+    def handle_start_activity(self, message):
+        print(message['data'])
+
+    def handle_end_activity(self, message):
+        print(message['data'])
+
+    def handle_emotion(self, message):
+        print(message['data'])
 
 
 if __name__ == '__main__':
-    call_component()
+    # call_component()
+    redis_cli = redis.Redis(port=REDIS_PORT, host=REDIS_HOST)
+    personalisation_tool = PersonalisationTool(redis_cli)
+    personalisation_tool.run()
