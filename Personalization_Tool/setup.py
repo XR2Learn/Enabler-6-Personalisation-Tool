7995e5017610bd952d6275b45ed370ff2fa30b4c
Annanda Sousa <annanda.sousa@gmail.com>
2024-10-28 09:46:24 +0100

    Bump version: 0.2.3 → 1.0.0

--- Personalization_Tool/setup.py
+++ Personalization_Tool/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='personalization_tool',
-    version='0.2.3',
+    version='1.0.0',
     description='Personalisation Tool (Enabler 6)',
     author='UM-XR2Learn-Enablers',
     packages=['personalization_tool'],
